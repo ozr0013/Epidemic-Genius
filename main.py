a5fa1682d1a84e5292ea5a67965f0788c30eb2df
atm0091 <117473472+atm0091@users.noreply.github.com>
2023-04-04 13:38:53 -0500

    CHANGE

--- main.py
+++ main.py
@@ -1,63 +1,74 @@
 import pandas as pd
+import tkinter as tk
 
-# Define function to simulate the spread of a disease over time
-def outcome (population, infected, duration, spreadRate, recoveryRate):
-    # Iterate over the number of days specified by duration
-    for day in range (1, duration + 1):
-        # Calculate the number of individuals who have recovered on this day
-        recovered = int(infected * recoveryRate)
-        # Subtract the number of recovered individuals from the infected population
-        infected = int(infected - recovered)
-        # Calculate the number of new infections on this day based on the spread rate
-        # and add them to the infected population
-        infected = int((population * spreadRate) + infected)
-        # Check if everyone is infected and print a message if true
-        # Calculate the infection rate as a percentage
-        infection_rate = (infected / population) * 100
-        if infection_rate >= 10:
-            quarantine_level = "Level 4 - Full Lockdown."
-        elif infection_rate >= 5:
-            quarantine_level = "Level 3 - Strict Quarantine. Enforce safety protocols to " \
-                               "prevent the need of a full lockdown"
-        elif infection_rate >= 2:
-            quarantine_level = "Level 2 - Moderate Quarantine. "
-        elif infection_rate >= 1:
-            quarantine_level = "Level 1 - Basic Quarantine. Consider enforcing stricter protocols to stop the spread."
-        else:
-            quarantine_level = "No quarantine needed"
-        if (infected > population):
-            print("Everyone is infected");
-        # Otherwise, print the number of infected and recovered individuals for this day
-        else:
-            print(f'Day {day}:\n\tInfected: {infected}\n\tRecovered: {recovered}\n\tQuarantine Level: {quarantine_level}')
-    # End of function
-
-# Main function
-if __name__ == "__main__":
+def get_population():
     # Read input data from CSV file
-    dataset = pd.read_csv('worldpopulation.csv', delimiter ='\t', header = 0)
-    # Ask user to input a country name
-    countryName = input("Enter a country name: ");
+    dataset = pd.read_csv('worldpopulation.csv', delimiter='\t', header=0)
     # Look up the population of the specified country in the dataset
     for index, row in dataset.iterrows():
-        if countryName == row['country']:
-            population = int(dataset.loc[index, 'population']);
-            print(f'{countryName}: {population}');
-    # Ask user to input the initial number of infected individuals
-    check = False;
-    while (check != True):
-        infected = int(input(f'Enter initial infected population amount (must be less than {int(population * .10)}): '))
-        # Check if the input is within the allowed range and prompt the user to try again if not
-        if (infected > population * 10 or infected < 0):
-            print('Illegal amount of infected')
-            check = False
-        else:
-            check = True
-    # Ask user to input the spread rate and recovery rate
-    spreadRate = float(input("Enter rate of spread per day: "))
-    recoveryRate = float(input("Enter rate of recovery per day: "))
-    # Ask user to input the duration of the simulation
-    duration = int(input("Enter Time Span (< 7 days): "))
+        if country.get() == row['country']:
+            return int(dataset.loc[index, 'population'])
+    return 0
 
-    # Call the outcome function with the specified parameters
-    outcome(population, infected, duration, spreadRate, recoveryRate)
+def calculate():
+    # Get input values from entry fields
+    countryName = country.get()
+    infected = int(infected_input.get())
+    spreadRate = float(spread_rate_input.get())
+    recoveryRate = float(recovery_rate_input.get())
+    duration = int(duration_input.get())
+
+    # Get population of specified country from CSV file
+    population = get_population()
+
+    # Calculate the number of infected individuals for each day of the simulation
+    infected_list = [infected]
+    for day in range(1, duration):
+        new_infected = int(infected_list[day-1] * spreadRate)
+        recovered = int(infected_list[day-1] * recoveryRate)
+        infected = max(infected_list[day-1] + new_infected - recovered, 0)
+        infected_list.append(infected)
+
+    # Display the results in the output label
+    output_label.config(text=f'{countryName}: {population}\nInfected: {infected_list}')
+
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Simulation")
+
+# Create entry fields for user input
+country_label = tk.Label(window, text="Country:")
+country_label.grid(row=0, column=0)
+country = tk.Entry(window)
+country.grid(row=0, column=1)
+
+infected_label = tk.Label(window, text="Initial Infected Population:")
+infected_label.grid(row=1, column=0)
+infected_input = tk.Entry(window)
+infected_input.grid(row=1, column=1)
+
+spread_rate_label = tk.Label(window, text="Rate of Spread per Day:")
+spread_rate_label.grid(row=2, column=0)
+spread_rate_input = tk.Entry(window)
+spread_rate_input.grid(row=2, column=1)
+
+recovery_rate_label = tk.Label(window, text="Rate of Recovery per Day:")
+recovery_rate_label.grid(row=3, column=0)
+recovery_rate_input = tk.Entry(window)
+recovery_rate_input.grid(row=3, column=1)
+
+duration_label = tk.Label(window, text="Duration of Simulation:")
+duration_label.grid(row=4, column=0)
+duration_input = tk.Entry(window)
+duration_input.grid(row=4, column=1)
+
+# Create a button to start the simulation
+calculate_button = tk.Button(window, text="Calculate", command=calculate)
+calculate_button.grid(row=5, column=0, columnspan=2)
+
+# Create a label to display the results
+output_label = tk.Label(window, text="")
+output_label.grid(row=6, column=0, columnspan=2)
+
+# Start the Tkinter event loop
+window.mainloop()
