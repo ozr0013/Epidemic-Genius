7de51aaf82a3840fac928696ca9cf4200143e909
atm0091 <117473472+atm0091@users.noreply.github.com>
2023-04-18 13:42:55 -0500

    Updated code with more info and extra window

--- chart.py
+++ chart.py
@@ -1,23 +1,22 @@
-import csv
 import matplotlib.pyplot as plt
-import diseaseSpread
 
-with open('worldpopulation.csv') as population_file:
-    reader = csv.DictReader(population_file)
-    infected_people = []
-    population_total = []
+#with open('worldpopulation.csv') as population_file:
+    #reader = csv.DictReader(population_file)
+    #infected_people = []
+    #population_total = []
 
-    for row in reader:
-        infected_people.append(int(row['infected'].strip()))
-        population_total.append(int(row['population'].strip()))
+    #for row in reader:
+       # infected_people.append(int(row['infected'].strip()))
+        #population_total.append(int(row['population'].strip()))
 
-days_list = range(1, len(diseaseSpread.infected_list) + 1)
+def makePlot(infected_list):
+    days_list = range(1, len(infected_list) + 1)
 
-plt.plot(days_list, diseaseSpread.infected_list)
-plt.title('Number of Infected People over Time')
-plt.xlabel('Days')
-plt.ylabel('Number of Infected People')
-plt.show()
+    plt.plot(days_list, infected_list)
+    plt.title('Number of Infected People over Time')
+    plt.xlabel('Days')
+    plt.ylabel('Number of Infected People')
+    return plt
 #Days = range(1, 366)  # assuming the data is for a leap year
 #plt.plot(Days, infected_people, label="Infected")
 #plt.plot(Days, population_total, label="Population")
--- diseaseSpread.py
+++ diseaseSpread.py
@@ -1,6 +1,10 @@
-import pandas as pd
 import tkinter as tk
-import chart
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pandas as pd
+from PIL import ImageTk, Image
+from tkinter import Label
 
 def get_population():
     # Read input data from CSV file
@@ -15,6 +19,7 @@ def calculate():
     # Get input values from entry fields
     countryName = country.get()
     infected = int(infected_input.get())
+    initialInfected = infected
     spreadRate = float(spread_rate_input.get())
     recoveryRate = float(recovery_rate_input.get())
     duration = int(duration_input.get())
@@ -24,87 +29,100 @@ def calculate():
 
     # Calculate the number of infected individuals for each day of the simulation
     infected_list = [infected]
-    totalPercentage = []
-    recoveredPeople = []
-    for day in range(1, duration):
-        new_infected = int(infected_list[day-1] * spreadRate)
-        recovered = int(infected_list[day-1] * recoveryRate)
-        recoveredPeople.append(recovered)
-        infected = max(infected_list[day-1] + new_infected - recovered, 0)
+    for day in range(duration):
+        new_infected = int(infected_list[day] * spreadRate)
+        recovered = int(infected_list[day] * recoveryRate)
+        infected = max(infected_list[day] + new_infected - recovered, 0)
         infected_list.append(infected)
 
-    length = len(infected_list)
-    for num in range(0, length):
-        totalPercentage.append(str(round(infected_list[num] / population * 100000, 2)) + '%')
     # Create a new window to display the results
     result_window = tk.Toplevel(window)
-    result_window.geometry("1000x800")
     result_window.title("Simulation Results")
 
-    # Define colors and font for the label
-    bg_color = "#f2f2f2"  # light gray background
-    fg_color = "#333333"  # dark gray foreground
-    font = ("Arial", 16, "bold")  # font family, size and weight
+    # Calculate the screen size and center the window
+    screen_width = result_window.winfo_screenwidth()
+    screen_height = result_window.winfo_screenheight()
+    window_width = 800
+    window_height = 600
+    x = int((screen_width - window_width) / 2)
+    y = int((screen_height - window_height) / 2)
+    result_window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-    # Create a label to display the results
-    new_output_label = tk.Label(result_window, text=f'{countryName}: \nPopulation of Infected '
-                                                    f'(each element represents a day): '
-                                                    f'{infected_list}'
-                                                    f'\nInfected Percentages (per day): {totalPercentage}'
-    # will be 1 less array length because first day does
-    # not have recovered value
-                                                    f'\nPeople Recovered Each Day: {recoveredPeople}',
-                                bg=bg_color, fg=fg_color, font=font)
-    new_output_label.pack(pady=50)
+    # Create a matplotlib figure to display the graph
+    figure = plt.Figure(figsize=(6, 5), dpi=100)
+    ax = figure.add_subplot(111)
+    ax.plot(infected_list, marker='o', linestyle='-', linewidth=2)
+    ax.set_title(f'{countryName}: Infected Population Over Time', fontsize=16)
+    ax.set_xlabel('Days', fontsize=14)
+    ax.set_ylabel('Infected Population', fontsize=14)
 
-    # Center the label in the middle of the window
-    result_window.update_idletasks()
-    x = (result_window.winfo_width() - new_output_label.winfo_width()) // 2
-    y = (result_window.winfo_height() - new_output_label.winfo_height()) - 999 // 2
-    new_output_label.place(x=x, y=y)
+    # Add the matplotlib figure to the Tkinter window
+    canvas = FigureCanvasTkAgg(figure, result_window)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    description_window = tk.Tk()
+    description_window.title("Description")
+    description_window.geometry("800x200")
+    text_box = tk.Label(description_window, text = f"This chart models {countryName} after {duration} days with"
+                                                   f"an infection rate of {spreadRate} and a\nrecover rate of {recoveryRate}."
+                                                   f"The initial amount of infected individuals was {initialInfected}."
+                                                   f" Each day, the\namount of recovered and newly infected will "
+                                                   f"be calculated based off"
+                                                   f"the current infected amount,\nthen the recovered people will be subtracted "
+                                                   f"from the total infected, and the new infected will be added\n"
+                                                   f" This goes on for as many days"
+                                                   f" as inputted.\n The end amount of total infected is {infected_list[-1]}.")
+    text_box.configure(font=("Arial", 16), justify = 'center', anchor = 'center', padx = 10, pady = 20)
+    text_box.pack()
 
 # Create a Tkinter window
 window = tk.Tk()
-window.geometry("750x400")
 window.title("Disease Spread Simulation")
+
 
 # Calculate label font size based on window size
 label_font_size = int(window.winfo_reqheight() / 10)
 
 # Create entry fields for user input
-country_label = tk.Label(window, text="Country:", font=("TkDefaultFont", label_font_size))
+country_label = tk.Label(window, text="Country:", font=("TkDefaultFont", label_font_size), fg = 'gray7')
 country_label.grid(row=0, column=0, padx=10, pady=10, sticky="W")
-country = tk.Entry(window, font=("TkDefaultFont", label_font_size))
+country = tk.Entry(window, font=("TkDefaultFont", label_font_size), fg = 'gray7')
 country.grid(row=0, column=1, padx=10, pady=10, sticky="W")
+country.configure(bg='gray63')
+country_label.configure(bg='gray63')
 
-infected_label = tk.Label(window, text="Initial Infected Population:", font=("TkDefaultFont", label_font_size))
+infected_label = tk.Label(window, text="Initial Infected Population:", font=("TkDefaultFont", label_font_size), fg = 'gray7')
 infected_label.grid(row=1, column=0, padx=10, pady=10, sticky="W")
-infected_input = tk.Entry(window, font=("TkDefaultFont", label_font_size))
+infected_input = tk.Entry(window, font=("TkDefaultFont", label_font_size), fg = 'gray7')
 infected_input.grid(row=1, column=1, padx=10, pady=10, sticky="W")
+infected_input.configure(bg='gray63')
+infected_label.configure(bg = 'gray63')
 
-spread_rate_label = tk.Label(window, text="Rate of Spread per Day:", font=("TkDefaultFont", label_font_size))
+spread_rate_label = tk.Label(window, text="Rate of Spread per Day:", font=("TkDefaultFont", label_font_size), fg = 'gray7')
 spread_rate_label.grid(row=2, column=0, padx=10, pady=10, sticky="W")
-spread_rate_input = tk.Entry(window, font=("TkDefaultFont", label_font_size))
+spread_rate_input = tk.Entry(window, font=("TkDefaultFont", label_font_size), fg = 'gray7')
 spread_rate_input.grid(row=2, column=1, padx=10, pady=10, sticky="W")
+spread_rate_input.configure(bg = 'gray63')
+spread_rate_label.configure(bg='gray63')
 
-recovery_rate_label = tk.Label(window, text="Rate of Recovery per Day:", font=("TkDefaultFont", label_font_size))
+recovery_rate_label = tk.Label(window, text="Rate of Recovery per Day:", font=("TkDefaultFont", label_font_size), fg = 'gray7')
 recovery_rate_label.grid(row=3, column=0, padx=10, pady=10, sticky="W")
-recovery_rate_input = tk.Entry(window, font=("TkDefaultFont", label_font_size))
+recovery_rate_input = tk.Entry(window, font=("TkDefaultFont", label_font_size), fg = 'gray7')
 recovery_rate_input.grid(row=3, column=1, padx=10, pady=10, sticky="W")
+recovery_rate_input.configure(bg='gray63')
+recovery_rate_label.configure(bg='gray63')
 
-duration_label = tk.Label(window, text="Duration of Simulation:", font=("TkDefaultFont", label_font_size))
+duration_label = tk.Label(window, text="Duration of Simulation (in days):", font=("TkDefaultFont", label_font_size), fg = 'gray7')
 duration_label.grid(row=4, column=0, padx=10, pady=10, sticky="W")
-duration_input = tk.Entry(window, font=("TkDefaultFont", label_font_size))
+duration_input = tk.Entry(window, font=("TkDefaultFont", label_font_size), fg = 'gray7')
 duration_input.grid(row=4, column=1, padx=10, pady=10, sticky="W")
+duration_input.configure(bg='gray63')
+duration_label.configure(bg='gray63')
 
 # Create a button to start the simulation
 calculate_button = tk.Button(window, text="Calculate", command=calculate)
 calculate_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
-
-
-# Create a label to display the results
-output_label = tk.Label(window, text="", font=("TkDefaultFont", label_font_size))
-output_label.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
 # Center all labels and entry fields
 for child in window.winfo_children():
@@ -113,6 +131,14 @@ for child in window.winfo_children():
 
 window.columnconfigure(0, weight=1)
 window.columnconfigure(1, weight=1)
+
+window.update_idletasks()
+width = window.winfo_width()
+height = window.winfo_height()
+x = (window.winfo_screenwidth() // 2) - (width // 2)
+y = (window.winfo_screenheight() // 2) - (height // 2)
+window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+window.configure(bg='steel blue')
 
 # Start the Tkinter event loop
 window.mainloop()
